e3aa33f8937c53cb055a6d9ad584006decfb5a20
Wiley E. Coyote <bill.wilson.home@gmail.com>
2018-05-26 19:01:22 -0400

    added Position class

--- db.py
+++ db.py
@@ -1,6 +1,10 @@
 import psycopg2
-import sys
 import psycopg2.extras
+
+import chess
+import chess.svg
+
+from heatmap import Heatmap
  
 class Connection:
     conn_string = "host='localhost' dbname='chess' user='www-data' password='NULL'"
@@ -15,20 +19,172 @@ class Connection:
         return cls.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
     @classmethod
-    def execute(cls, cursor, query):
-        cursor.execute(query)
+    def execute(cls, cursor, query, *params):
+        cursor.execute(query, params)
         return cursor.rowcount, cursor
+
 
 class Query(Connection):
 
     @classmethod
     def random_search(cls):
         c = Connection.cursor()
-        return cls.execute(c, "select * from random_search()")
+        n, rows = cls.execute(c, "select * from random_search()")
+        for row in rows:
+            yield PositionResult(row)
+
+    @classmethod
+    def random_position(cls):
+        c = Connection.cursor()
+        n, rows = cls.execute(c, "select * from v_position order by random() limit 1")
+        if n == 0:
+            return None
+        for row in rows:
+            return Position(row)
+
+    @classmethod
+    def select_fen(cls, fen):
+
+        c = Connection.cursor()
+        n, rows = cls.execute(c, "select * from v_position where fen=%s", fen)
+        if n == 0:
+            return None
+        for row in rows:
+            return Position(row)
+
+
+class Position(Connection):
+    '''
+    DROP VIEW IF EXISTS v_position;
+    CREATE VIEW v_position AS
+        SELECT 
+             site
+            ,fen
+            ,side(fen)
+            ,score
+            ,scores
+            ,pieces(fen)::square[] AS squares
+            ,pieces(fen)::cpiece[]
+            ,keypieces::square[] AS keysquares
+            ,keypieces::cpiece[]
+        FROM POSITION p
+    ;
+    '''
+    heatgen = Heatmap()
+
+    def _iter_array(self, array):
+
+        for piece in array[1:-1].split(','):
+            yield piece
+
+    def _parse_pieces(self, pieces):
+        l = []
+        for p in self._iter_array(pieces):
+            l.append(chess.Piece.from_symbol(p))
+        return l
+
+    def _parse_squares(self, squares):
+        l = []
+        for s in self._iter_array(squares):
+            l.append(chess.SQUARE_NAMES.index(s))
+        return l
+
+
+    def __init__(self, row):
+        self.id = None
+        self.site = row['site']
+        self.board = chess.Board(row['fen'] + ' 1 1')
+        self.side = row['side']
+        self.score = row['score']
+        self.scores = row['scores']
+
+        self.squares = self._parse_squares(row['squares'])
+        self.pieces = self._parse_pieces(row['pieces'])
+        self.keysquares = self._parse_squares(row['keysquares'])
+        self.keypieces = self._parse_pieces(row['keypieces'])
+
+        self.heatmap = {}
+        for square, color in zip(self.keysquares, self.heatgen.gen(self)):
+            name = chess.SQUARE_NAMES[square]
+            self.heatmap[name] = color
+
+    def __str__(self):
+        return "<Position '{}'>".format(self.board.fen())
+
+    def squareset(self, piecesquares):
+        #board = chess.Board(board_fen=None)
+        s = chess.SquareSet(0)
+        for piece in self._iter_array(piecesquares):
+            p = chess.Piece.from_symbol(piece[0])
+            sq = chess.SQUARE_NAMES.index(piece[1:])
+            #board.set_piece_at(d, p)
+            s.add(sq)
+        return s
+    
+    def svg(self, id=None, **kwargs):
+        sl = '<rect class="square light {}" fill="#ffce9e"'
+        sd = '<rect class="square dark {}" fill="#d18b47"'
+
+        rl = '<rect class="square light {}" fill="{}"'
+        rd = '<rect class="square dark {}" fill="{}"'
+
+        style = ''
+
+        circles = []
+        for square in self.keysquares:
+            circles.append((square, square))
+
+        txt = chess.svg.board(self.board, arrows=circles, style=style, coordinates=False, **kwargs)
+
+        for key, val in self.heatmap.items():
+            txt = txt.replace(sl.format(key), rl.format(key, val))
+            txt = txt.replace(sd.format(key), rd.format(key, val))
+
+        if id:
+            txt = txt.replace("<svg ", '<svg id="{}"'.format(id))
+        return txt
+
+class PositionResult(Position):
+    '''
+    DROP TYPE t_search_result cascade;
+    CREATE TYPE t_search_result AS
+    (
+         queryboard board
+        ,querykey   piecesquare[]
+        ,distance   double precision
+        ,site       text
+        ,fen        board
+        ,side       side
+        ,score      real
+        ,scores     real[]
+        ,squares    square[]
+        ,pieces     cpiece[]
+        ,keysquares square[]
+        ,keypieces  cpiece[]
+    );
+    '''
+    def __init__(self, row):
+        super().__init__(row)
+        self.distance = round(row['distance'], 3)
+        self.querykey = row['querykey']
+        self.queryboard = chess.Board(row['queryboard'] + ' 1 1')
+
+    def __str__(self):
+        return "<PositionResult '{}'>".format(self.board.fen())
+
+
 
 Connection.connect()
-for row in (Query.random_search())[1]:
-    print(row)
+#p = Query.select_fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+#for row in (Query.random_search())[1]:
+#    print(PositionResult(row))
 
-    
+pos = Query.random_position()
+print(pos.scores)
+print(pos.squares)
+print(pos.pieces)
+#print(pos.svg())
+#print(heat.gen(pos))
+#
+
 
--- server.py
+++ server.py
@@ -5,9 +5,11 @@ from sanic import response
 from jinja2 import Template
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-import db
+import chess
+import chess.svg
 
-ROOT = '/home/lukehand/src/chess/web/sanic/'
+import db
+ROOT = '/home/lukehand/src/chess/web/'
 
 env = Environment(
     loader=FileSystemLoader(ROOT + 'views/'),
@@ -18,7 +20,28 @@ import random
 def randomize():
     return '?' + str(random.randint(1, 1000000))
 
+def svg(pos, id=None, keysquares=None, size=400):
+    return pos.svg(id=pos.id, size=size)
+
+def svg_fen(board, querykey=[], size=400, **kwargs):
+
+    circles = []
+    for square in querykey:
+        circles.append((square, square))
+
+    txt = chess.svg.board(board, arrows=circles, size=size, **kwargs)
+    return txt
+
+def fmt_ps(pieces, squares):
+    l = []
+    for p, s in zip(pieces, squares):
+        l.append("{}{}".format(p, chess.SQUARE_NAMES[s]))
+    return '{' + ','.join(l) + '}'
+
 env.globals['randomize'] = randomize
+env.globals['svg'] = svg
+env.globals['svg_fen'] = svg_fen
+env.globals['fmt_ps'] = fmt_ps
 
 app = Sanic()
 app.static('/s/', './static')
@@ -26,15 +49,27 @@ app.static('/img/', './static/img')
 
 @app.route("/")
 async def test(request):
-    n, results = db.Query.random_search()
 
+    pos = db.Query.random_position()
+    t = env.get_template("single.jinja")
+    return response.html(t.render(pos=pos))
+
+@app.route("/search")
+async def search(request):
     l = []
-    for row in results:
+    for i, row in enumerate(db.Query.random_search()):
+        row.id = i
         l.append(row)
-    query = l[0]['query']
+    query = l[0].queryboard
+    querytxt = l[0].querykey
+
+    querykey = []
+    print(44, l[0].querykey)
+    for p in l[0].querykey[1:-1].split(","):
+        querykey.append(chess.SQUARE_NAMES.index(p[1:]))
 
     t = env.get_template("index.jinja")
-    return response.html(t.render(query=query, results=l))
+    return response.html(t.render(query=query, querykey=querykey, querytxt=querytxt, results=l))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
