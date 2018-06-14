395ccee09cf9102b185c54950c0dfc8ab4a0d821
Wiley E. Coyote <bill.wilson.home@gmail.com>
2018-06-14 15:34:42 -0400

    added Game class

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import time
 
 #package
 import psycopg2
@@ -11,7 +12,22 @@ import chess
 import sys
 sys.path.append('../pgchess')
 from db import Connection
+from svgboard import SvgBoard
+
 Connection.connect('www-data')
+
+def timeit(f):
+
+    def timed(*args, **kw):
+
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        sys.stderr.write('func:%r args:[%r, %r] took: %2.4f sec\n' % \
+          (f.__name__, args, kw, te-ts))
+        return result
+    return timed
 
 class Query(Connection):
 
@@ -32,6 +48,13 @@ class Query(Connection):
             return Position(row)
 
     @classmethod
+    def random_game(cls):
+        c = Connection.cursor()
+        n, rows = cls.execute("select * from v_game order by random() limit 1")
+        if n:
+            return Game(rows.fetchone())
+
+    @classmethod
     def select_fen(cls, fen):
 
         c = Connection.cursor()
@@ -42,8 +65,48 @@ class Query(Connection):
 
     @classmethod
     def canonical_pawns(cls):
-        n, rows = cls.execute('SELECT n, board FROM kmode ORDER BY pcount(board) DESC, pawn_distance(board)')
-        return rows.fetchall()
+        n, rows = cls.execute('SELECT * from m_kmode_agg order by count desc')
+        for row in rows:
+            yield Centroid(row)
+
+    @classmethod
+    @timeit
+    def kmode_positions(self, id):
+        n, rows = Connection.execute('select * from v_kmode where pclass=%s order by random() limit 20', id)
+        l = []
+        for row in rows:
+            l.append(CentroidPos(row))
+        return l
+
+class Centroid:
+
+    def __init__(self, row):
+        self.count = row['count']
+        self.pclass = row['pclass']
+        self.avg = row['avg']
+        self.stddev = row['stddev']
+        self.board = row['pawn_centr']
+
+        if 'win' in row.keys():
+            self.win = round(row['win'], 2)
+            self.draw = row['draw']
+            self.lose = row['lose']
+            self.rating = int(row['rating'])
+
+    @timeit
+    def to_svg(self, size, href=None):
+        return self.board.to_svg(size, href=href).tostring()
+
+
+class CentroidPos(Centroid):
+    def __init__(self, row):
+        super().__init__(row)
+        self.similar = row['distance']
+        self.position = row['pawns']
+
+    @timeit
+    def to_svg(self, size):
+        return self.position.to_svg(size, comp=self.board, title=self.similar).tostring()
 
 
 class Position(Connection):
@@ -64,7 +127,7 @@ class Position(Connection):
         self.scores = row['scores']
         self.keypieces = row['keypieces']
         self.mobility = row['mobility']
-        self.diff = row['diff']
+        #self.diff = row['diff']
         #self.attacks = row['attacks']
 
         if self.board.turn:
@@ -75,6 +138,24 @@ class Position(Connection):
     def __str__(self):
         return "<Position '{}'>".format(self.board.fen())
 
+    def __iter__(self):
+        return self.piecesquares().__iter__()
+
+    @property
+    def movenum(self):
+        return self.board.fullmove_number
+
+    @property
+    def halfmovenum(self):
+        o = 1 if self.board.turn == chess.BLACK else 0
+        return (self.board.fullmove_number-1) * 2 + o
+
+    @property
+    def fen(self):
+        return self.board.fen()
+    
+    def piecesquares(self):
+        return [(t[1], t[0]) for t in list(self.board.piece_map().items())]
 
     def squareset(self, piecesquares):
         #board = chess.Board(board_fen=None)
@@ -86,25 +167,35 @@ class Position(Connection):
             s.add(sq)
         return s
 
-    def to_svg(self, size, query=None, labels=False):
-        svg = SvgBoard(size=size, labels=labels)
+    def to_svg(self, size, comp=None, title=None, caption=None, legend=True, fen=True, labels=False):
+        return self.board.to_svg(size, comp=comp, title=title, legend=legend, fen=fen, labels=labels).tostring()
 
-        for score, (square, piece) in zip(self.scores, self._iter_piecesquares()):
-            svg.add_piece(square, piece)
-            #if square in [k.square for k in self.keypieces]:
-            #    if 0:#piecesquareif hasattr(self, 'querykey') and square in [s.square for s in self.querykey]:
-            #        svg.add_circle(square, stroke='green')
-            #    else:
-            #        svg.add_circle(square)
-            if query:
-                for square, val in self.occupancy_map(query).items():
-                    if val > 0:
-                     svg.add_circle(square, stroke='green')
-                for square, val in self.mobility_map(query).items():
-                    if val > 0:
-                         svg.set_square_color(square, self.green[val])
-                    if val < 0:
-                         svg.set_square_color(square, self.red[val])
+        svg = SvgBoard(size=size, labels=labels)
+        if title:
+            svg.add_title(title)
+        if caption:
+            svg.add_caption(title)
+        else:
+            svg.add_caption("{}".format(self.board.fen()))
+        for p,s in self.piecesquares():
+            svg.add_piece(s, p)
+
+        #for score, (square, piece) in zip(self.scores, self._iter_piecesquares()):
+        #    svg.add_piece(square, piece)
+        #    #if square in [k.square for k in self.keypieces]:
+        #    #    if 0:#piecesquareif hasattr(self, 'querykey') and square in [s.square for s in self.querykey]:
+        #    #        svg.add_circle(square, stroke='green')
+        #    #    else:
+        #    #        svg.add_circle(square)
+        #    if query:
+        #        for square, val in self.occupancy_map(query).items():
+        #            if val > 0:
+        #             svg.add_circle(square, stroke='green')
+        #        for square, val in self.mobility_map(query).items():
+        #            if val > 0:
+        #                 svg.set_square_color(square, self.green[val])
+        #            if val < 0:
+        #                 svg.set_square_color(square, self.red[val])
 
             #color = self.heatmapgen.color(score)
             #if color:
@@ -113,8 +204,7 @@ class Position(Connection):
         svg.add_legend()
         if not hasattr(self, 'distance'):
             self.distance=None
-        svg.add_title("{} {} {} {}".format(self.side, self.distance, round(self.score*.01, 2), self.site))
-        svg.add_caption("{}".format(self.board.fen()))
+        #svg.add_title("{} {} {} {}".format(self.side, self.distance, round(self.score*.01, 2), self.site))
         #svg.add_arrow(0, 16)
         #svg.add_title("HELLO", 'http://example.com')
         #svg.add_caption("bye")
@@ -151,6 +241,13 @@ class Position(Connection):
 
         return d
     
+class PositionMG(Position):
+    def __init__(self, row):
+        super().__init__(row)
+        self.similar = round(row['similar'], 3)
+
+    def __str__(self):
+        return "<PositionResult '{}'>".format(self.board.fen())
 
 class PositionResult(Position):
     def __init__(self, row):
@@ -162,11 +259,81 @@ class PositionResult(Position):
     def __str__(self):
         return "<PositionResult '{}'>".format(self.board.fen())
 
-l = []
-for row in Query.canonical_pawns():
-    l.append(row)
-print(l)
+class Game:
+    '''
+ gameid        | bigint       |           |          | 
+ event         | text         |           |          | 
+ site          | text         |           |          | 
+ date_         | date         |           |          | 
+ round         | integer      |           |          | 
+ wplayer       | text         |           |          | 
+ bplayer       | text         |           |          | 
+ result        | gameresult   |           |          | 
+ eco           | character(3) |           |          | 
+ eco_name      | text         |           |          | 
+ variation     | text         |           |          | 
+ wdiff         | integer      |           |          | 
+ bdiff         | integer      |           |          | 
+ wrating       | integer      |           |          | 
+ brating       | integer      |           |          | 
+ time_control  | text         |           |          | 
+ moves         | text         |           |          | 
+ mg_halfmove   | integer      |           |          | 
+ eg_halfmove   | integer      |           |          | 
+ last_halfmove | integer      |           |          | 
+    '''
 
+    def __init__(self, row):
+        self.site = row['site']
+        self.date = row['date_']
+        self.event = row['event']
+        self.wplayer = row['wplayer']
+        self.bplayer = row['bplayer']
+        self.result = row['result']
+        self.eco = row['eco']
+        self.eco_name = row['eco_name']
+        self.variation = row['variation']
+        self.wdiff = row['wdiff']
+        self.bdiff = row['bdiff']
+        self.wrating = row['wrating']
+        self.brating = row['brating']
+        self.time_control = row['time_control']
+        self.moves = row['moves']
+        self.mg_halfmove = row['mg_halfmove']
+        self.eg_halfmove = row['eg_halfmove']
+        self.pawn_centr = row['pawn_centr']
+        self.pclass = row['pclass']
 
+    def __str__(self):
+        return "<Game '{}'>".format(self.title())
+    
+    def __iter__(self):
+        for p in self.positions():
+            yield p
 
+    @classmethod
+    def get(cls, site):
+        n, rows = Connection.execute('select * from v_game where site=%s', site)
+        if n:
+            return cls(rows.fetchone())
 
+    def positions(self):
+        n, rows = Connection.execute('select * from v_position_mg where site=%s order by halfmove(fen)', self.site)
+        l = []
+        for row in rows:
+            l.append(PositionMG(row))
+        return l
+
+    def title(self):
+        return '{} ({}) vs {} ({}) {}'.format(self.wplayer, self.wdiff, self.bplayer, self.bdiff, self.result)
+
+    def subtitle(self):
+        return '{} {} - {}: {}'.format(self.event, self.time_control, self.eco, self.eco_name)
+
+#l = []
+#for row in Query.canonical_pawns():
+#    l.append(row)
+#print(l)
+
+game = Query.random_game()
+print(game)
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 
 import random
+import time
+import sys
+import os
 
 #package
 from sanic import Sanic
@@ -14,12 +17,35 @@ import chess.svg
 
 #local
 from model import Query
+from model import Game
+
 
 ROOT = '/home/lukehand/src/chess/web/'
+
+loader=FileSystemLoader(ROOT + 'views/')
 env = Environment(
-    loader=FileSystemLoader(ROOT + 'views/'),
+    loader=loader,
     autoescape=select_autoescape(['html', 'xml'])
 )
+#env.compile_templates(ROOT + '__pycache__/cache.tmp')
+
+def timeit(f):
+
+    def timed(*args, **kw):
+
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+
+        sys.stderr.write('func:%r args:[%r, %r] took: %2.4f sec\n' % \
+          (f.__name__, args, kw, te-ts))
+        return result
+
+    return timed
+
+@timeit
+def time_render(temp, *args, **kwargs):
+    return temp.render(*args, **kwargs)
 
 def randomize():
     return '?' + str(random.randint(1, 1000000))
@@ -36,7 +62,20 @@ app = Sanic()
 app.static('/s/', './static')
 app.static('/img/', './static/img')
 app.static('/svg/', './static/svg')
+app.static('/js/', './static/js')
 app.static('/favicon.ico', './static/img/favicon.ico')
+app.static('/scipy/', '../scipy/img/')
+
+@app.middleware('request')
+async def add_start_time(request):
+    request['start_time'] = time.time()
+
+
+@app.middleware('response')
+async def add_spent_time(request, response):
+    spend_time = round((time.time() - request['start_time']) * 1000)
+    sys.stderr.write("timing: {} {} {} {} {}ms\n".format(response.status, request.method,
+                                           request.path, request.query_string, spend_time))
 
 @app.route("/")
 async def index(request):
@@ -47,7 +86,17 @@ async def index(request):
 async def kmode(request):
     t = env.get_template("kmode.jinja")
     rows = Query.canonical_pawns()
-    return response.html(t.render(rows=rows))
+    return response.html(time_render(t, rows=rows))
+
+@app.route("/kmode/<id>")
+async def kmode_pos(request, id):
+    l = Query.kmode_positions(id)
+    if not l:
+        t = env.get_template("no_results.jinja")
+        return response.html(t.render())
+    else:
+        t = env.get_template("positions.jinja")
+        return response.html(time_render(t, rows=l))
 
 @app.route("/search")
 async def search(request):
@@ -61,6 +110,19 @@ async def search(request):
         query = Query.select_fen(l[0].queryboard.fen())
         t = env.get_template("index.jinja")
         return response.html(t.render(query=query, results=l))
+
+@app.route("/game")
+async def game(request):
+
+    g = Query.random_game()
+    game = Game.get(g.site)
+
+    if not game:
+        t = env.get_template("no_results.jinja")
+        return response.html(t.render())
+    else:
+        t = env.get_template("game.jinja")
+        return response.html(t.render(game=game))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
